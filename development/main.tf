5723c4e695ac9b9416f224ac33936d62e42578f9
Thorsten Hans <thorsten.hans@gmail.com>
2019-12-03 14:47:16 +0100

    live coding happened

---
+++ development/main.tf
@@ -0,0 +1,13 @@
+module "infra" {
+  source           = "../infrastructure"
+  environment_name = "development-live"
+  asp_size         = "S1"
+  custom_tags = {
+    author = "Thorsten"
+  }
+  location = "westeurope"
+}
+
+output "website_url" {
+  value = module.infra.app_service_url
+}
